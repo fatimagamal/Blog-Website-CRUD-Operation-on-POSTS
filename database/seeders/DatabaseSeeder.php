7dad64f0d12165dccba977031a0ba066b804afc7
Fatma-Gamal <gamalfatma351@gmail.com>
2022-02-02 21:15:28 +0200

    add seeders to lap2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(500)->create();
+
+       \App\Models\Post::factory(500)->create();
+
     }
 }
